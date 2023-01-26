9a37f428300c67229e22f298d9c7316d4bd65e01
Thanawat Udompholvanich <78265311+Salagoza@users.noreply.github.com>
2023-01-26 20:14:49 +0700

    Cleaned Code

--- product.py
+++ product.py
@@ -3,25 +3,26 @@ from db import db
 from models import Product
 from sqlalchemy.exc import SQLAlchemyError
 
-product = Blueprint('product', __name__)
+product_blueprint = Blueprint('product', __name__)
 
 """
 Function to create product.
 """
-@product.route("/create", methods=["POST"])
-def create():
+@product_blueprint.route("/create", methods=["POST"])
+def create_product():
     name = request.form["name"]
-    type = request.form["type"]
+    category = request.form["type"]
     price = request.form["price"]
     machine_id = request.form["machine_id"]
     quantity = request.form["quantity"]
-    # If the new product already exists in the machine increase the stock else create insert as a new record.
+
     product = Product.query.filter(Product.name == name, Product.machine_id == machine_id).first()
     if product is None:
-        new_product = Product(name, type, price, machine_id, quantity)
+        new_product = Product(name, category, price, machine_id, quantity)
         db.session.add(new_product)
     else:
         product.quantity += quantity
+
     try:
         db.session.commit()
     except SQLAlchemyError:
@@ -32,32 +33,32 @@ def create():
 """
 Function to get all products.
 """
-@product.route("/get", methods=["GET"])
-def get():
-    lists = list(map(lambda l: l.to_dict(), Product.query.all()))
-    return lists, 200
+@product_blueprint.route("/get", methods=["GET"])
+def get_all_products():
+    response = list(map(lambda l: l.to_dict(), Product.query.all()))
+    return response, 200
 
 """
 Function to get product by id.
 """
-@product.route("/get/<id>", methods=["GET"])
-def get_by_id(id):
-    product = Product.query.get(id)
+@product_blueprint.route("/get/<product_id>", methods=["GET"])
+def get_product_by_id(product_id: int):
+    product = Product.query.get(product_id)
     if product is None:
         return "No such product exists in the database!", 404
-    return product.to_dict(), 200
+    response = product.to_dict()
+    return response, 200
 
 """
 Function to update the product information.
 """
-@product.route("/update/<id>", methods=["PUT"])
-def update(id):
-    product = Product.query.get(id)
+@product_blueprint.route("/update/<product_id>", methods=["PUT"])
+def update_product(product_id: int):
+    product = Product.query.get(product_id)
     if product is None:
         return "No such product exists in the database!", 404
-
     new_name = request.form["name"] or product.name
-    new_type = request.form["type"] or product.type
+    new_type = request.form["type"] or product.category
     new_quantity = request.form["quantity"] or product.quantity
     new_price = request.form["price"] or product.price
 
@@ -65,7 +66,7 @@ def update(id):
         return "The quantity must be greater than 1!", 400
 
     product.name = new_name
-    product.type = new_type
+    product.category = new_type
     product.quantity = new_quantity
     product.price = new_price
 
@@ -79,19 +80,20 @@ def update(id):
 """
 Function to delete product.
 """
-@product.route("/delete", methods=["DELETE"])
-def delete():
+@product_blueprint.route("/delete", methods=["DELETE"])
+def delete_product():
     name = request.form["name"]
     machine_id = request.form["machine_id"]
     quantity = int(request.form["quantity"])
     product = Product.query.filter(Product.name == name, Product.machine_id == machine_id).first()
     if product is None:
         return "No such product exists in the database", 404
-    # If the stock level of that product is greater than 1 decrease the stock else delete the record.
+
     if product.quantity > quantity:
         product.quantity -= quantity
     else:
         db.session.delete(product)
+
     try:
         db.session.commit()
     except SQLAlchemyError:
