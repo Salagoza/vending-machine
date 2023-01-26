eb2f972bba7ac26c29bc33a70dc31a2276beba78
Thanawat Udompholvanich <78265311+Salagoza@users.noreply.github.com>
2023-01-26 23:16:16 +0700

    Add tests for machine and product

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask
-
 from db import db
 from machine import machine_blueprint
 from product import product_blueprint
 
 DB_NAME = "database.db"
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db.init_app(app)
+def create_app(database_uri=f"sqlite:///{DB_NAME}"):
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    db.init_app(app)
 
-app.register_blueprint(machine_blueprint, url_prefix="/api/machine")
-app.register_blueprint(product_blueprint, url_prefix="/api/product")
-   
+    app.register_blueprint(machine_blueprint, url_prefix="/api/machine")
+    app.register_blueprint(product_blueprint, url_prefix="/api/product")
 
-with app.app_context():
-    db.create_all()
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+
---
+++ test_machine.py
@@ -0,0 +1,31 @@
+from models import Machine
+
+def test_create_machine(client, app):
+    client.post("/api/machine/create", data={"name": "Machine1", "address": "Tennis Court"})
+    with app.app_context():
+        assert Machine.query.count() == 1
+        assert Machine.query.first().name == "Machine1"
+
+def test_get_all_machine(client):
+    response = client.get("/api/machine/get")
+    assert response.status_code == 200
+
+def test_get_machine_by_id(client, app):
+    client.post("/api/machine/create", data={"name": "Machine1", "address": "Tennis Court"})
+    response = client.get("/api/machine/get/1")
+    assert response.status_code == 200
+
+def test_update_machine(client, app):
+    client.post("/api/machine/create", data={"name": "Machine1", "address": "Tennis Court"})
+    client.put("/api/machine/update/1", data={"name": "Machine2", "address": "Ic Building"})
+    with app.app_context():
+        assert Machine.query.count() == 1
+        assert Machine.query.first().name == "Machine2"
+        assert Machine.query.first().address == "Ic Building"
+
+def test_delete_machine(client, app):
+    client.post("/api/machine/create", data={"name": "Machine1", "address": "Tennis Court"})
+    client.delete("/api/machine/delete/1")
+
+    with app.app_context():
+        assert Machine.query.count() == 0
